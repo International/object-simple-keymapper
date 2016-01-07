735e30264b76c88b8fde7688273eb195be010c7c
George Opritescu <ssscripting@gmail.com>
2016-01-07 21:11:11 +0100

    tests passing

--- spec/keymapper_spec.js
+++ spec/keymapper_spec.js
@@ -3,7 +3,7 @@ var KeyMapper = require("../"),
 
 describe("KeyMapper",function() {
   "use strict";
-  it("maps property x to y on a object", function() {
+  it("maps property x to y on an object", function() {
     var a = {x:20};
     var b = {};
     KeyMapper.performMapping(a, b, {
@@ -13,4 +13,31 @@ describe("KeyMapper",function() {
     });
     should.exist(b.y);
   });
+
+  it("takes only properties x and y from an object", function() {
+    var a = {x:20, y:20, z:30};
+    var b = {};
+    KeyMapper.performMapping(a, b, {
+      only: ["x","y"],
+    });
+    should.exist(b.x);
+    should.exist(b.y);
+    should.not.exist(b.z);
+  });
+
+  it("takes only properties x and y from an object, and remaps y as z", function() {
+    var a = {x:20, y:20, z:30};
+    var b = {};
+    KeyMapper.performMapping(a, b, {
+      only: ["x","y"],
+      remap: {
+        y: "z"
+      }
+    });
+    should.exist(b.x);
+    should.exist(b.z);
+    should.not.exist(b.y);
+  });
+
+
 });
